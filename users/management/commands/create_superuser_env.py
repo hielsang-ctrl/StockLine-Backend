1f12487195221dae3af0cc53709c19690f2da943
hiel <hiel.sang@student.moringaschool.com>
2026-07-26 12:22:09 +0300

    Add create_superuser_env management command

---
+++ users/management/commands/create_superuser_env.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = 'Create superuser from environment variables if it does not exist'
+
+    def handle(self, *args, **kwargs):
+        from decouple import config
+        email = config('SUPERUSER_EMAIL', default='')
+        password = config('SUPERUSER_PASSWORD', default='')
+        username = config('SUPERUSER_USERNAME', default='admin')
+
+        if not email or not password:
+            self.stdout.write('SUPERUSER_EMAIL or SUPERUSER_PASSWORD not set, skipping.')
+            return
+
+        if User.objects.filter(email=email).exists():
+            self.stdout.write(f'Superuser {email} already exists, skipping.')
+            return
+
+        User.objects.create_superuser(username=username, email=email, password=password, role='admin')
+        self.stdout.write(self.style.SUCCESS(f'Superuser {email} created.'))
